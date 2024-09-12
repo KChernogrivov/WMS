b016721d0292415e6aab986f8f87a60e5b208f2c
Черногривов Кирилл <kirill.2201@inbox.ru>
2024-09-13 00:37:18 +0700

    all list pages

--- wms-project/src/pages/warehouse/warehouses-page.vue
+++ wms-project/src/pages/warehouse/warehouses-page.vue
@@ -1,13 +1,70 @@
 <template>
-  <div class="orders-page">
-    orders
-  </div>
+  <v-card
+    title="Orders list"
+    flat
+  >
+    <template v-slot:text>
+      <v-text-field
+        v-model="search"
+        label="Search"
+        prepend-inner-icon="mdi-magnify"
+        variant="outlined"
+        hide-details
+        single-line
+      ></v-text-field>
+    </template>
+
+    <v-data-table
+      hover
+      :headers="headers"
+      :items="collection"
+      :search="search"
+      :loading="loading"
+      @click:row="openOrder"
+    >
+      <template v-slot:loading>
+        <v-skeleton-loader type="table-row@10"></v-skeleton-loader>
+      </template>
+    </v-data-table>
+  </v-card>
 </template>
 
 <script setup>
+import {onMounted, ref} from "vue";
+import {orderController} from "@/shared/utils/api/orderController/orderController";
+import router from "@/app/router";
 
+const loading = ref(true);
+const search = ref();
+
+onMounted(async () => {
+  try {
+    const response = await orderController.getOrders();
+    collection.value = response.data.orders;
+  } catch (error) {
+    console.log(error)
+  } finally {
+    loading.value = false;
+  }
+})
+
+const headers = [
+  {
+    title: 'Reference',
+    align: 'start',
+    sortable: false,
+    key: 'reference',
+  },
+  {title: 'Description', key: 'description', align: 'end'},
+  {title: 'Status', key: 'status.name', align: 'end'},
+];
+
+const collection = ref([]);
+
+function openOrder(event, row) {
+  router.push(`/orders/${row.item.id}`);
+}
 </script>
-
 <style scoped>
 
 </style>
